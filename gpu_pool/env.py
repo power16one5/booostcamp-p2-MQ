7b7e566e3305ff1448c9d8653da1068d8b0010fc
Kyung won <power16one5@naver.com>
2024-01-04 18:05:44 +0900

    feat: env.py에 비어있는 항목들을 수정했습니다

--- gpu_pool/env.py
+++ gpu_pool/env.py
@@ -2,18 +2,18 @@ import os
 
 CWD = os.getcwd()
 
-MYSQL_SERVER_IP =
-MYSQL_SERVER_PORT =
-MYSQL_SERVER_USER =
-MYSQL_SERVER_PASSWORD =
+MYSQL_SERVER_IP = '10.28.224.124'
+MYSQL_SERVER_PORT = 30156
+MYSQL_SERVER_USER = 'teemo'
+MYSQL_SERVER_PASSWORD = 'teemo'
 
-MESSAGE_FILE_PATH =
+MESSAGE_FILE_PATH = './message.json'
 
 # id, branch, train_path, pushed의 위치를 수정하지 마세요.
 MESSAGE_COLUMNS = ['id', 'branch', 'train_path',
-                   
-                  '...',
-
+                   'name', 'seed', 'epoch', 'dataset', 'augmentation',
+                   'criterion', 'batch_size',
+                   'model', 'optimizer', 
                   'pushed']
 
 # 이하 수정 x
